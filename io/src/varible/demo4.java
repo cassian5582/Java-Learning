e007d3c6be8a8d897534a17bdeed7289ec80bacc
cassian5582 <t248415174@163.com>
2026-05-08 14:24:21 +0800

    5/8

---
+++ io/src/varible/demo4.java
@@ -0,0 +1,9 @@
+package varible;
+
+public class demo4 {
+    public static void main(String[] args) {
+        double t = Math.random();
+        System.out.println(t);
+        System.out.format("%f",t);
+    }
+}
